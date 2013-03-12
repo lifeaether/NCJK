139b8b0aa16bb395e3b8d977baa45b49ee39941d
lifeaether <lifeaether@gmail.com>
2013-03-12 22:56:54 +0900

    Initial Commit

---
+++ NCJK/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NCJK
+//
+//  Created by lifeaether on 2013/03/12.
+//  Copyright (c) 2013年 lifeaether. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
